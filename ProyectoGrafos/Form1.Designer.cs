1e72ada88671da4a039117ce81cafa44bffffd97
rogerfor <rogerfor7@gmail.com>
2012-06-01 20:01:16 -0600

    Proyecto Grafos

---
+++ ProyectoGrafos/Form1.Designer.cs
@@ -0,0 +1,472 @@
+﻿namespace ProyectoGrafos
+{
+    partial class Grafos
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.AA = new System.Windows.Forms.TextBox();
+            this.AB = new System.Windows.Forms.TextBox();
+            this.AC = new System.Windows.Forms.TextBox();
+            this.AD = new System.Windows.Forms.TextBox();
+            this.BD = new System.Windows.Forms.TextBox();
+            this.BC = new System.Windows.Forms.TextBox();
+            this.BB = new System.Windows.Forms.TextBox();
+            this.BA = new System.Windows.Forms.TextBox();
+            this.CD = new System.Windows.Forms.TextBox();
+            this.CC = new System.Windows.Forms.TextBox();
+            this.CB = new System.Windows.Forms.TextBox();
+            this.CA = new System.Windows.Forms.TextBox();
+            this.DD = new System.Windows.Forms.TextBox();
+            this.DC = new System.Windows.Forms.TextBox();
+            this.DB = new System.Windows.Forms.TextBox();
+            this.DA = new System.Windows.Forms.TextBox();
+            this.ED = new System.Windows.Forms.TextBox();
+            this.EC = new System.Windows.Forms.TextBox();
+            this.EB = new System.Windows.Forms.TextBox();
+            this.EA = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.EE = new System.Windows.Forms.TextBox();
+            this.DE = new System.Windows.Forms.TextBox();
+            this.CE = new System.Windows.Forms.TextBox();
+            this.BE = new System.Windows.Forms.TextBox();
+            this.AE = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar Grafo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "D";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "E";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(235, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "E";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(195, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "D";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(160, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "C";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(131, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "B";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(98, 51);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "A";
+            // 
+            // AA
+            // 
+            this.AA.Location = new System.Drawing.Point(92, 72);
+            this.AA.Name = "AA";
+            this.AA.Size = new System.Drawing.Size(29, 20);
+            this.AA.TabIndex = 11;
+            this.AA.Text = "0";
+            // 
+            // AB
+            // 
+            this.AB.Location = new System.Drawing.Point(127, 72);
+            this.AB.Name = "AB";
+            this.AB.Size = new System.Drawing.Size(29, 20);
+            this.AB.TabIndex = 12;
+            this.AB.Text = "0";
+            // 
+            // AC
+            // 
+            this.AC.Location = new System.Drawing.Point(162, 72);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(29, 20);
+            this.AC.TabIndex = 13;
+            this.AC.Text = "0";
+            // 
+            // AD
+            // 
+            this.AD.Location = new System.Drawing.Point(197, 72);
+            this.AD.Name = "AD";
+            this.AD.Size = new System.Drawing.Size(29, 20);
+            this.AD.TabIndex = 14;
+            this.AD.Text = "0";
+            // 
+            // BD
+            // 
+            this.BD.Location = new System.Drawing.Point(197, 98);
+            this.BD.Name = "BD";
+            this.BD.Size = new System.Drawing.Size(29, 20);
+            this.BD.TabIndex = 18;
+            this.BD.Text = "0";
+            // 
+            // BC
+            // 
+            this.BC.Location = new System.Drawing.Point(162, 98);
+            this.BC.Name = "BC";
+            this.BC.Size = new System.Drawing.Size(29, 20);
+            this.BC.TabIndex = 17;
+            this.BC.Text = "0";
+            // 
+            // BB
+            // 
+            this.BB.Location = new System.Drawing.Point(127, 98);
+            this.BB.Name = "BB";
+            this.BB.Size = new System.Drawing.Size(29, 20);
+            this.BB.TabIndex = 16;
+            this.BB.Text = "0";
+            // 
+            // BA
+            // 
+            this.BA.Location = new System.Drawing.Point(92, 98);
+            this.BA.Name = "BA";
+            this.BA.Size = new System.Drawing.Size(29, 20);
+            this.BA.TabIndex = 15;
+            this.BA.Text = "0";
+            // 
+            // CD
+            // 
+            this.CD.Location = new System.Drawing.Point(197, 124);
+            this.CD.Name = "CD";
+            this.CD.Size = new System.Drawing.Size(29, 20);
+            this.CD.TabIndex = 22;
+            this.CD.Text = "0";
+            // 
+            // CC
+            // 
+            this.CC.Location = new System.Drawing.Point(162, 124);
+            this.CC.Name = "CC";
+            this.CC.Size = new System.Drawing.Size(29, 20);
+            this.CC.TabIndex = 21;
+            this.CC.Text = "0";
+            // 
+            // CB
+            // 
+            this.CB.Location = new System.Drawing.Point(127, 124);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(29, 20);
+            this.CB.TabIndex = 20;
+            this.CB.Text = "0";
+            // 
+            // CA
+            // 
+            this.CA.Location = new System.Drawing.Point(92, 124);
+            this.CA.Name = "CA";
+            this.CA.Size = new System.Drawing.Size(29, 20);
+            this.CA.TabIndex = 19;
+            this.CA.Text = "0";
+            // 
+            // DD
+            // 
+            this.DD.Location = new System.Drawing.Point(197, 150);
+            this.DD.Name = "DD";
+            this.DD.Size = new System.Drawing.Size(29, 20);
+            this.DD.TabIndex = 26;
+            this.DD.Text = "0";
+            // 
+            // DC
+            // 
+            this.DC.Location = new System.Drawing.Point(162, 150);
+            this.DC.Name = "DC";
+            this.DC.Size = new System.Drawing.Size(29, 20);
+            this.DC.TabIndex = 25;
+            this.DC.Text = "0";
+            // 
+            // DB
+            // 
+            this.DB.Location = new System.Drawing.Point(127, 150);
+            this.DB.Name = "DB";
+            this.DB.Size = new System.Drawing.Size(29, 20);
+            this.DB.TabIndex = 24;
+            this.DB.Text = "0";
+            // 
+            // DA
+            // 
+            this.DA.Location = new System.Drawing.Point(92, 150);
+            this.DA.Name = "DA";
+            this.DA.Size = new System.Drawing.Size(29, 20);
+            this.DA.TabIndex = 23;
+            this.DA.Text = "0";
+            // 
+            // ED
+            // 
+            this.ED.Location = new System.Drawing.Point(198, 177);
+            this.ED.Name = "ED";
+            this.ED.Size = new System.Drawing.Size(29, 20);
+            this.ED.TabIndex = 30;
+            this.ED.Text = "0";
+            // 
+            // EC
+            // 
+            this.EC.Location = new System.Drawing.Point(163, 177);
+            this.EC.Name = "EC";
+            this.EC.Size = new System.Drawing.Size(29, 20);
+            this.EC.TabIndex = 29;
+            this.EC.Text = "0";
+            // 
+            // EB
+            // 
+            this.EB.Location = new System.Drawing.Point(128, 177);
+            this.EB.Name = "EB";
+            this.EB.Size = new System.Drawing.Size(29, 20);
+            this.EB.TabIndex = 28;
+            this.EB.Text = "0";
+            // 
+            // EA
+            // 
+            this.EA.Location = new System.Drawing.Point(93, 177);
+            this.EA.Name = "EA";
+            this.EA.Size = new System.Drawing.Size(29, 20);
+            this.EA.TabIndex = 27;
+            this.EA.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(41, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(250, 20);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "Ingresar matriz de adyacencía";
+            // 
+            // EE
+            // 
+            this.EE.Location = new System.Drawing.Point(233, 177);
+            this.EE.Name = "EE";
+            this.EE.Size = new System.Drawing.Size(29, 20);
+            this.EE.TabIndex = 36;
+            this.EE.Text = "0";
+            // 
+            // DE
+            // 
+            this.DE.Location = new System.Drawing.Point(232, 150);
+            this.DE.Name = "DE";
+            this.DE.Size = new System.Drawing.Size(29, 20);
+            this.DE.TabIndex = 35;
+            this.DE.Text = "0";
+            // 
+            // CE
+            // 
+            this.CE.Location = new System.Drawing.Point(232, 124);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(29, 20);
+            this.CE.TabIndex = 34;
+            this.CE.Text = "0";
+            // 
+            // BE
+            // 
+            this.BE.Location = new System.Drawing.Point(232, 98);
+            this.BE.Name = "BE";
+            this.BE.Size = new System.Drawing.Size(29, 20);
+            this.BE.TabIndex = 33;
+            this.BE.Text = "0";
+            // 
+            // AE
+            // 
+            this.AE.Location = new System.Drawing.Point(232, 72);
+            this.AE.Name = "AE";
+            this.AE.Size = new System.Drawing.Size(29, 20);
+            this.AE.TabIndex = 32;
+            this.AE.Text = "0";
+            // 
+            // Grafos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 273);
+            this.Controls.Add(this.EE);
+            this.Controls.Add(this.DE);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.BE);
+            this.Controls.Add(this.AE);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ED);
+            this.Controls.Add(this.EC);
+            this.Controls.Add(this.EB);
+            this.Controls.Add(this.EA);
+            this.Controls.Add(this.DD);
+            this.Controls.Add(this.DC);
+            this.Controls.Add(this.DB);
+            this.Controls.Add(this.DA);
+            this.Controls.Add(this.CD);
+            this.Controls.Add(this.CC);
+            this.Controls.Add(this.CB);
+            this.Controls.Add(this.CA);
+            this.Controls.Add(this.BD);
+            this.Controls.Add(this.BC);
+            this.Controls.Add(this.BB);
+            this.Controls.Add(this.BA);
+            this.Controls.Add(this.AD);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.AB);
+            this.Controls.Add(this.AA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Grafos";
+            this.Text = "Grafos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox AA;
+        private System.Windows.Forms.TextBox AB;
+        private System.Windows.Forms.TextBox AC;
+        private System.Windows.Forms.TextBox AD;
+        private System.Windows.Forms.TextBox BD;
+        private System.Windows.Forms.TextBox BC;
+        private System.Windows.Forms.TextBox BB;
+        private System.Windows.Forms.TextBox BA;
+        private System.Windows.Forms.TextBox CD;
+        private System.Windows.Forms.TextBox CC;
+        private System.Windows.Forms.TextBox CB;
+        private System.Windows.Forms.TextBox CA;
+        private System.Windows.Forms.TextBox DD;
+        private System.Windows.Forms.TextBox DC;
+        private System.Windows.Forms.TextBox DB;
+        private System.Windows.Forms.TextBox DA;
+        private System.Windows.Forms.TextBox ED;
+        private System.Windows.Forms.TextBox EC;
+        private System.Windows.Forms.TextBox EB;
+        private System.Windows.Forms.TextBox EA;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox EE;
+        private System.Windows.Forms.TextBox DE;
+        private System.Windows.Forms.TextBox CE;
+        private System.Windows.Forms.TextBox BE;
+        private System.Windows.Forms.TextBox AE;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ProyectoGrafos/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProyectoGrafos
+{
+    public partial class Grafos : Form
+    {
+        public Grafos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string fileName = "grafo.dot";
+            FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter grafo = new StreamWriter(stream);
+
+            grafo.WriteLine("digraph A {");
+            /*A*/
+            if (Convert.ToInt16(AA.Text).Equals(1))
+            {
+                grafo.WriteLine("A -> A;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(AB.Text).Equals(1))
+            {
+                grafo.WriteLine("A -> B;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(AC.Text).Equals(1))
+            {
+
+                grafo.WriteLine("A -> C;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(AD.Text).Equals(1))
+            {
+
+                grafo.WriteLine("A -> D;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(AE.Text).Equals(1))
+            {
+
+                grafo.WriteLine("A -> E;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            /*B*/
+            if (Convert.ToInt16(BA.Text).Equals(1))
+            {
+                grafo.WriteLine("B -> A;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(BB.Text).Equals(1))
+            {
+                grafo.WriteLine("B -> B ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(BC.Text).Equals(1))
+            {
+
+                grafo.WriteLine("B -> C ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(BD.Text).Equals(1))
+            {
+
+                grafo.WriteLine("B -> D ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(BE.Text).Equals(1))
+            {
+
+                grafo.WriteLine("B -> E ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            /*C*/
+            if (Convert.ToInt16(CA.Text).Equals(1))
+            {
+                grafo.WriteLine("C -> A;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(CB.Text).Equals(1))
+            {
+                grafo.WriteLine("C -> B ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(CC.Text).Equals(1))
+            {
+
+                grafo.WriteLine("C -> C ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(CD.Text).Equals(1))
+            {
+
+                grafo.WriteLine("C -> D ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(CE.Text).Equals(1))
+            {
+
+                grafo.WriteLine("C -> E ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            /*D*/
+            if (Convert.ToInt16(DA.Text).Equals(1))
+            {
+                grafo.WriteLine("D -> A;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(DB.Text).Equals(1))
+            {
+                grafo.WriteLine("D -> B ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(DC.Text).Equals(1))
+            {
+
+                grafo.WriteLine("D -> C ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(DD.Text).Equals(1))
+            {
+
+                grafo.WriteLine("D -> D ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(DE.Text).Equals(1))
+            {
+
+                grafo.WriteLine("D -> E ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            /*E*/
+            if (Convert.ToInt16(EA.Text).Equals(1))
+            {
+                grafo.WriteLine("E -> A;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(EB.Text).Equals(1))
+            {
+                grafo.WriteLine("E -> B ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(EC.Text).Equals(1))
+            {
+
+                grafo.WriteLine("E -> C ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(ED.Text).Equals(1))
+            {
+
+                grafo.WriteLine("E -> D ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            if (Convert.ToInt16(EE.Text).Equals(1))
+            {
+
+                grafo.WriteLine("E -> E ;");
+            }
+            else
+            {
+                grafo.WriteLine("");
+            }
+            grafo.WriteLine("}");
+            grafo.Close();
+
+
+            /**Guardar y Crear la Imagen*/
+           
+                ProcessStartInfo Grafo = new ProcessStartInfo("dot.exe");
+                Grafo.Arguments = "grafo.dot -o grafo.png -Tpng -Grankdir=LR";
+                Grafo.UseShellExecute = true;
+                Process.Start(Grafo);
+            
+            
+        }
+    }
+}
